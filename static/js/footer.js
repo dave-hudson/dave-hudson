76ab959158c4d810f26b57faffa5d5d56b901429
Dave Hudson <dave-hudson@users.noreply.github.com>
2020-02-09 23:49:06 +0000

    Add dark/light mode toggle
    * Allow the blog reader to select light or dark mode.
    
    * By default the site will use the system defaults for light/dark mode.

---
+++ static/js/footer.js
@@ -0,0 +1,61 @@
+var darkTheme = document.getElementById("dark-mode-theme");
+var darkModeSun = document.getElementById("dark-mode-sun");
+var darkModeMoon = document.getElementById("dark-mode-moon");
+
+function setTheme(mode) {
+    if (mode === "dark") {
+        darkModeSun.style.display = "inline";
+        darkModeMoon.style.display = "none";
+        darkTheme.disabled = false;
+    } else {
+        darkModeSun.style.display = "none";
+        darkModeMoon.style.display = "inline";
+        darkTheme.disabled = true;
+    }
+}
+
+// If the user clicks the sun icon then flip to lisht mode.
+darkModeSun.addEventListener("click", () => {
+    console.log("click ");
+    if (darkModeSun.style.display === "none") {
+        setTheme("dark");
+    } else {
+        setTheme("light");
+    }
+});
+
+// If the user clicks the moon icon then flip to dark mode.
+darkModeMoon.addEventListener("click", () => {
+    console.log("click ");
+    if (darkModeMoon.style.display === "none") {
+        setTheme("light");
+    } else {
+        setTheme("dark");
+    }
+});
+
+// If we can, work out whether we should default to dark or light mode.
+const windowMedia = window.matchMedia("(prefers-color-scheme: dark)");
+if (windowMedia.matches) {
+    setTheme("dark");
+} else {
+    setTheme("light");
+}
+
+if (windowMedia.addEventListener) {
+    windowMedia.addEventListener("change", () => {
+        if (windowMedia.matches) {
+            setTheme("dark");
+        } else {
+            setTheme("light");
+        }
+    });
+} else if (windowMedia.addListener) {
+    windowMedia.addListener(() => {
+        if (windowMedia.matches) {
+            setTheme("dark");
+        } else {
+            setTheme("light");
+        }
+    });
+}
